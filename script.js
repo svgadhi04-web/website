1af8c0863a8d11dd590422bf29b4cf70b2d4b5c7
svgadhi04-web <svgadhi04@gmail.com>
2025-11-28 15:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle mobile menu
+function toggleMenu() {
+    document.getElementById("nav-links").classList.toggle("show");
+}
+
+// Button click animation/message
+function buttonClick() {
+    alert("Thanks for clicking!");
+}
